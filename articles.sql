1bcbc8b1ec08583118f95151ad728b895c7b7cce
kgoespiritu <kgoespiritu@gmail.com>
2015-04-25 21:03:25 +0800

    update article table
    change column name to non reserved words

--- articles.sql
+++ articles.sql
@@ -5,6 +5,7 @@
                  MySQL 5.6 Database
  */
 
+--------------------------------------------------------------------
 CREATE TABLE article
 (
 `id`           INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -18,3 +19,32 @@ CREATE TABLE article
 );
 
 ALTER TABLE article ADD COLUMN `thumb_file` VARCHAR(50) AFTER `text`;
+---------------------------------------------------------------------
+
+USE test;
+CREATE TABLE article
+(
+id         INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+author_id    INT UNSIGNED NOT NULL DEFAULT 0,
+category_id  INT UNSIGNED NOT NULL DEFAULT 0,
+active       BOOL NOT NULL DEFAULT 0,
+title        VARCHAR(50) NOT NULL,
+record_text         TEXT,
+thumb_file   VARCHAR(50),
+record_date         datetime DEFAULT CURRENT_TIMESTAMP
+);
+
+----------------------------------------------------------------------
+
+/*
+PARTITION BY RANGE ( YEAR(`date`) )
+SUBPARTITION BY HASH ( MONTH(`date`) )
+SUBPARTITIONS 12
+(
+PARTITION p0 VALUES LESS THAN (2000),
+PARTITION p1 VALUES LESS THAN (2005),
+PARTITION p2 VALUES LESS THAN (2010),
+PARTITION p3 VALUES LESS THAN (2015),
+PARTITION p4 VALUES LESS THAN (2020)
+);
+*/
