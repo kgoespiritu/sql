21d6f9b47d27a9f52afe78c9743d253db6db394e
kgoespiritu <kgoespiritu@gmail.com>
2015-04-25 14:16:22 +0800

    update user table add active

--- users.sql
+++ users.sql
@@ -8,5 +8,8 @@
 CREATE TABLE users
 (
 `id`          INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+`active`      BOOL NOT NULL DEFAULT 0,
 `username`    VARCHAR(50)
 );
+
+ALTER TABLE users ADD COLUMN `active` BOOL NOT NULL DEFAULT 0 AFTER `id`
