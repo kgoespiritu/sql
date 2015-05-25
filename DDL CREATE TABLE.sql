3b0c36ec7c57b91b1e3e54988d808db912e64937
kgoespiritu <kgoespiritu@gmail.com>
2015-05-25 13:40:41 +0800

    CREATE TABLE updates add headers

--- DDL CREATE TABLE.sql
+++ DDL CREATE TABLE.sql
@@ -15,34 +15,42 @@ select_statement
 =======================
 { LIKE old_tbl_name | ( LIKE old_tbl_name )}
 
-create_definition:
+/* ================== */
+/* create_definition: */
+/* ================== */
 col_name column_definition
 [ CONSTRAINT [ symbol ] ] PRIMARY KEY [ index_type ] ( index_col_name , ... ) [ index_option ] ...
 { INDEX | KEY } [ index_name ] [ index_type ] ( index_col_name , ... ) [ index_option ] ...
 [ CONSTRAINT [ sumbol ] ] UNIQUE [ INDEX | KEY ] [ index_name ] [ index_type ] ( index_col_name , ... ) [ index_option ] ...
 { FULLTEXT | SPATIAL } [ INDEX | KEY ] [ index_name ] ( index_col_name , ... ) [ index_option ] ...
-[ CONSTRAINT [ symbol ] ] FOREIGN KEY [ index_name ] ( index_col_name , ... ) reference_definition
+[ CONSTRAINT [ symbol ] ] FOREIGN KEY [ index_name ] ( index_col_name , ... ) reference_definition /* unpartitoned InnoDB OR NDB + KEY + LINEAR KEY only */
 CHECK( expr )
 
-column_definition:
+/* ================== */
+/* column_definition: */
+/* ================== */
 data_type
 [ NOT NULL | NULL ]
 [ DEFAULT default_value ]
 [ AUTO_INCREMENT ]
 [ UNIQUE [ KEY ] | [ PRIMARY ] KEY ]
 [ COMMENT 'string' ]
-[ COLUMN_FORMAT { FIXED | DYNAMIC | DEFAULT } ]
-[ STORAGE { DISK | MEMORY | DEFAULT } ]
+[ COLUMN_FORMAT { FIXED | DYNAMIC | DEFAULT } ] /* NDB silent ignore */
+[ STORAGE { DISK | MEMORY | DEFAULT } ]         /* NDB only , syntax error */
 [ reference_definition ]
 
-data_type:
+/* ================== */
+/* data_type: */
+/* ================== */
 
-
-
-index_col_name:
+/* ================== */
+/* index_col_name: */
+/* ================== */
 col_name [ ( length ) ] [ ASC | DESC ]
 
+/* ================== */
 index_type:
+/* ================== */
 USING { BTREE | HASH }
 
 index_option:
@@ -51,44 +59,54 @@ index_type
 WITH_PARSER parser_name
 COMMENT 'string'
 
+/* ================== */
 reference_definition:
+/* ================== */
 REFERENCES tbl_name ( index_col_name , ... )
 [ MATCH FULL | MATCH PARTIAL | MATCH SIMPLE ]
 [ ON DELETE reference_option ]
 [ ON UPDATE reference_option ]
 
+/* ================== */
 reference_option :
+/* ================== */
 RESTRICT | CASCADE | SET NULL | NO ACTION
 
+/* ================== */
 table_options :
+/* ================== */
 table_option , ...
 
+/* ================== */
 table_option:
+/* ================== */
 ENGINE [=] engine_name
-AUTO_INCREMENT [=] value
+AUTO_INCREMENT [=] value /* MyISAM , MEMORY , InnoDB , ARCHIVE */
 AVG_ROW_LENGTH [=] value
 [ DEFAULT ] CHARACTER SET [=] charset_name
-CHECKSUM [=] { 0 | 1 }
+CHECKSUM [=] { 0 | 1 } /* MyISAM */
 [ DEFAULT ] COLLATE [=] collation_name
-COMMENT [=] 'string'
-CONNECTION [=] 'connection_string'
-DATA DIRECTORY [=] 'absolute path to directory'
-DELAY_KEY_WRITE [=] { 0 | 1 }
-INDEX DIRECTORY [=] 'absolute path to directory'
-INSERT_METHOD [=] { NO | FIRST | LAST }
-KEY_BLOCK_SIZE [=] value
-MAX_ROWS [=] value
-MIN_ROWS [=] value
-PACK_KEYS [=] { 0 | 1 | DEFAULT }
-PASSWORD [=] 'string'
-ROW_FORMAT [=] { DEFAULT | DYNAMIC | FIXED | COMPRESSED | REDUNDANT | COMPACT }
-STATS_AUTO_RECALC [=] { DEFAULT | 0 | 1 }
-STATS_PERSISTENT [=] { DEFAULT | 0 | 1 }
+COMMENT [=] 'string' /* 2048 characters */
+CONNECTION [=] 'connection_string' /* FEDERATED table SERVER */
+DATA DIRECTORY [=] 'absolute path to directory' /* InnoDB , MyISAM */
+DELAY_KEY_WRITE [=] { 0 | 1 } /* MyISAM */
+INDEX DIRECTORY [=] 'absolute path to directory' /* InnoDB , MyISAM */
+INSERT_METHOD [=] { NO | FIRST | LAST } /* MERGE */
+KEY_BLOCK_SIZE [=] value /* hint : InnoDB : 0 , 1 , 2 , 4 , 8 , 16 */
+MAX_ROWS [=] value /* hint : NDB : MAX 4294967295 */
+MIN_ROWS [=] value /* MEMORY */
+PACK_KEYS [=] { 0 | 1 | DEFAULT } /* MyISAM */
+PASSWORD [=] 'string' /* NOT USED */
+ROW_FORMAT [=] { DEFAULT | DYNAMIC | FIXED | COMPRESSED | REDUNDANT | COMPACT } /* storage_engine depeneden */
+STATS_AUTO_RECALC [=] { DEFAULT | 0 | 1 } /* InnoDB Stats */
+STATS_PERSISTENT [=] { DEFAULT | 0 | 1 } /* InnoDB Stats */
 STATS_SAMPLE_PAGES [=] value
-TABLESPACE tablespace_name [ STORAGE { DISK | MEMORY | DEFAULT } ]
-UNION [=] ( tbl_name , ... )
+TABLESPACE tablespace_name [ STORAGE { DISK | MEMORY | DEFAULT } ] /* NDB Only */
+UNION [=] ( tbl_name , ... ) /* only with MERGE tables */
 
+/* ================== */
 partition_options :
+/* ================== */
 PARTITION BY { 
 	[ LINEAR ] HASH( expr )
 	[ LINEAR ] KEY [ ALGORITHM = ( 1 | 2 ) ] ( column_list ) 
@@ -104,14 +122,16 @@ PARTITION BY {
 ]
 [ ( partition_definition , ... ) ]
 
+/* ================== */
 partition_definition:
+/* ================== */
 PARTITION partition_name
 	[ VALUES {
 		LESS THAN { ( expr | value_list ) }
 		IN ( value_list )
 		}
 	]
-	[ [ STORAGE ] ENGINE [=] engine_name ]
+	[ [ STORAGE ] ENGINE [=] engine_name ] /* NDB ? */
 	[ COMMENT [=] 'comment_text' ]
 	[ DATA DIRECTORY [=] 'data_dir' ]
 	[ INDEX DIRECTORY [=] 'index_dir' ]
@@ -120,10 +140,12 @@ PARTITION partition_name
 	[ TABLESPACE [=] tablespace_name ]
 	[ NODEGROUP [=] node_group_id ]
 	[ ( subpartition_definition , ... ) ]
-	
+
+/* ================== */
 subpartition_definition:
+/* ================== */
 SUBPARTITION logical_name
-	[ [ STORAGE ] ENGINE [=] engine_name ]
+	[ [ STORAGE ] ENGINE [=] engine_name ] /* NDB ? */
 [ COMMENT [=] 'comment_text' ]
 	[ DATA DIRECTORY [=] 'data_dir' ]
 	[ INDEX DIRECTORY [=] 'index_dir' ]
@@ -132,5 +154,21 @@ SUBPARTITION logical_name
 	[ TABLESPACE [=] tablespace_name ]
 	[ NODEGROUP [=] node_group_id ]
 
+/* ================== */
 select_statement:
-	[ IGNORE | REPLACE ] [ AS ] SELECT ...
+/* ================== */
+[ IGNORE | REPLACE ] [ AS ] SELECT ...
+
+/* ================== */
+storage_engine:
+/* ================== */
+InnoDB
+MyISAM
+MEMORY
+CSV
+ARCHIVE
+EXAMPLE
+FEDERATED
+HEAP
+MERGE
+NDB
